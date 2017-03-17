e437b10493a2b6fcd1bc7ff2a30e913d97ba7906
Anthony Chiu <ChiuA@MacBook-Pro.local>
2017-03-17 13:02:25 +0000

    Codacy fixes

--- js/sharedBulk.js
+++ js/sharedBulk.js
@@ -1,29 +1,16 @@
-function generateList(values, queryEnd, bulkType, firstUse)
-{
-  var arr = [];
-  // Form the query:
-  for(var x in values){
-      // Check to see if inputs are empty
-      if (values[x][1] !== ""){
-        if (bulkType === "multi" && firstUse){
-          arr.push("{\"request\": \"");
-          firstUse = false;
-        }
-        arr.push(values[x][0]);
-        arr.push(values[x][1]);
-        arr.push(" AND ");
-    }
-    else {
-    }
-  }
-  arr.pop();
-  arr.push(queryEnd);
-  query = arr.join(""); // Join the array with no seperator
-  if (query !== queryEnd && query !== ""){
-    addBulk(query, bulkType)
-  }
-}
+var multiBulkQuery = [];
 
+function createBulkList(toAdd){
+    for (var x = 0; x < multiBulkQuery.length; x++){
+      var replaced = multiBulkQuery[x].replace(/}|{|,|:|"|"|request/g,"");
+      index = x+1;
+      toAdd += "<h3>" + index.toString() + "." + replaced + "</h3>";
+      toAdd += "<button type='button' class='btn btn-primary' onclick='deleteBulk("+x.toString()+");'>Delete</button>";
+      toAdd += "<br>";
+  }
+  toAdd += "</div>";
+  document.getElementById("bulkList").innerHTML = toAdd;
+}
 
 /**
   * @function addBulk(bulkType)
@@ -43,17 +30,33 @@ function addBulk(query, bulkType){
   toAdd += "<label>List of Queries</label>";
   if (query !== "" || typeof query !== "undefined"){
       multiBulkQuery.push(query+ "\n");
-    }
-    for (var x = 1; x < multiBulkQuery.length; x++){
-      var replaced = multiBulkQuery[x].replace(/}|{|,|:|"|"|request/g,"");
-      toAdd += "<h3>" +x.toString() + "." + replaced + "</h3>";
-      toAdd += "<button type='button' class='btn btn-primary' onclick='deleteBulk("+x.toString()+");'>Delete</button>";
-      toAdd += "<br>";
   }
-  toAdd += "</div>";
-  document.getElementById("bulkList").innerHTML = toAdd;
-  }
+  createBulkList(toAdd);
+}
 
+function generateList(values, queryEnd, bulkType, firstUse)
+{
+  var arr = [];
+  // Form the query:
+  for(var x in values){
+      // Check to see if inputs are empty
+      if (values[x][1] !== ""){
+        if (bulkType === "multi" && firstUse){
+          arr.push("{\"request\": \"");
+          firstUse = false;
+        }
+        arr.push(values[x][0]);
+        arr.push(values[x][1]);
+        arr.push(" AND ");
+    }
+  }
+  arr.pop();
+  arr.push(queryEnd);
+  var query = arr.join(""); // Join the array with no seperator
+  if (query !== queryEnd && query !== ""){
+    addBulk(query, bulkType);
+  }
+}
 
 /**
   * @function deleteBulk (currentIndex, bulkType)
@@ -70,5 +73,5 @@ function deleteBulk(currentIndex){
   toAdd += "<div class='form-group'  id='bulkList' style='width: 25em; float:right; margin-right:20%'>";
   toAdd += "<label>List of Queries</label>";
   multiBulkQuery.splice(currentIndex,1);
-  createBulkList(toAdd,multiBulkQuery);
+  createBulkList(toAdd);
 }
